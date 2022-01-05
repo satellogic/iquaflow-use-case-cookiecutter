db70b7f4e2c1539efd63553ac7a56dceb9815bbe
Pau Galles <pau.galles@satellogic.com>
2022-01-05 11:09:09 +0000

    Update download.sh

--- {{ cookiecutter.repo_name }}/download.sh
+++ {{ cookiecutter.repo_name }}/download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #TO_PATH=$(PROJECT_NAME)/models
-#python3 -c "import os; os.makedirs('$(TO_PATH)/weights',exist_ok=True)"
+#python3 -c "import os; os.makedirs('$TO_PATH',exist_ok=True)"
 #wget https://image-quality-framework.s3-eu-west-1.amazonaws.com/weights/???.pth -O $TO_PATH/weights/???.pth
 
 #TO_PATH=.
